5a22a1d79b3bb9aa90898fa5bc49900dc5e1a10c
Jason Wang <jasonw80702@gmail.com>
2022-05-26 17:12:28 -0700

    feat(stats): force re-render on tab click

--- components/main/StatsScreen.js
+++ components/main/StatsScreen.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import { Text } from '@rneui/base';
 import { View } from 'react-native';
 
-const StatsScreen = () => {
+const useForceUpdate = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [value, setValue] = useState(0);
+  return () => setValue(value => value + 1);
+};
+
+const StatsScreen = ({ navigation }) => {
+  const forceUpdate = useForceUpdate();
+
+  useEffect(() => {
+    // This is for being able to render every time we click on the tab
+    const unsubscribe = navigation.addListener('tabPress', () => {
+      forceUpdate();
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
+  console.log('render');
   return (
     <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Text>Stats</Text>
+      <Text>Stats Page</Text>
     </View>
   );
+};
+
+StatsScreen.propTypes = {
+  navigation: PropTypes.any.isRequired,
 };
 
 export default StatsScreen;
