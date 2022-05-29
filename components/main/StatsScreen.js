5f23abc3f78f5f5b357dbbcfbb562f2c75cb56db
Jason Wang <jasonw80702@gmail.com>
2022-05-28 18:39:43 -0700

    feat(personal_stats): fetch data on bottom tab click * move logic from personal stats to stats screen

--- components/main/StatsScreen.js
+++ components/main/StatsScreen.js
@@ -1,17 +1,62 @@
-import React from 'react';
+/* eslint-disable react/no-children-prop */
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import PersonalStats from '../stats/PersonalStats';
+import { fetchUserDailys, setUserDailysLoading } from '../../redux/actions/index';
 import GlobalStats from '../stats/GlobalStats';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Tab = createMaterialTopTabNavigator();
 
-const StatsScreen = () => {
+const useForceUpdate = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [value, setValue] = useState(0);
+  return () => setValue(value => value + 1);
+};
+
+const StatsScreen = ({ navigation }) => {
+  const forceUpdate = useForceUpdate();
+  const dispatch = useDispatch();
+
+  const userDailys = useSelector((state) => state.userDailysState.userDailys);
+  const isLoadingUserDailys = useSelector((state) => state.userDailysState.isLoading);
+
+  useEffect(() => {
+    // This is for being able to render every time we click on the bottom tab
+    const unsubscribe = navigation.addListener('tabPress', () => {
+      forceUpdate();
+      dispatch(setUserDailysLoading());
+      dispatch(fetchUserDailys());
+    });
+
+    return unsubscribe;
+  }, [navigation, dispatch]);
+
+  useEffect(() => {
+    dispatch(setUserDailysLoading());
+    dispatch(fetchUserDailys());
+  }, [dispatch]);
+
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Personal" component={PersonalStats} />
-      <Tab.Screen name="Global" component={GlobalStats} />
+      <Tab.Screen name='Personal' children={
+        props =>
+          <PersonalStats
+            userDailys={userDailys}
+            isLoadingUserDailys={isLoadingUserDailys}
+            {...props}
+          />
+      } />
+      <Tab.Screen name='Global' component={GlobalStats} />
     </Tab.Navigator>
   );
+};
+
+StatsScreen.propTypes = {
+  navigation: PropTypes.shape({
+    addListener: PropTypes.func,
+  }),
 };
 
 export default StatsScreen;
--- components/stats/PersonalStats.js
+++ components/stats/PersonalStats.js
@@ -1,49 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { ListItem, Text } from '@rneui/base';
 import { View } from 'react-native';
-import { fetchUserDailys, setUserDailysLoading, fetchDailys, setDailysLoading } from '../../redux/actions/index';
+import { fetchDailys, setDailysLoading } from '../../redux/actions/index';
 import Loader from '../shared/Loader';
 import DailysSummary from './DailysSummary';
 import DailyOverlay from './DailyOverlay';
 
-// TODO: consider doing this in <StatsScreen /> and passing data here to props
-// https://stackoverflow.com/questions/60439210/how-to-pass-props-to-screen-component-with-a-tab-navigator
-const useForceUpdate = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [value, setValue] = useState(0);
-  return () => setValue(value => value + 1);
-};
-
 // TODO: wtf why are you doing 6 renders
 // TODO: let's do pagination for 2.0
-const PersonalStats = ({ navigation }) => {
+const PersonalStats = ({ isLoadingUserDailys, userDailys}) => {
   const dispatch = useDispatch();
-  const forceUpdate = useForceUpdate();
   const [showModal, setShowModal] = useState(false);
   const [currentUserDaily, setCurrentUserDaily] = useState(null);
 
-  const userDailys = useSelector((state) => state.userDailysState.userDailys);
-  const isLoadingUserDailys = useSelector((state) => state.userDailysState.isLoading);
   const dailys = useSelector((state) => state.dailysState.dailys);
   const isLoadingDailys = useSelector((state) => state.dailysState.isLoading);
-
-  useEffect(() => {
-    // This is for being able to render every time we click on the tab
-    const unsubscribe = navigation.addListener('tabPress', () => {
-      forceUpdate();
-      dispatch(setUserDailysLoading());
-      dispatch(fetchUserDailys());
-    });
-
-    return unsubscribe;
-  }, [navigation, dispatch]);
-
-  useEffect(() => {
-    dispatch(setUserDailysLoading());
-    dispatch(fetchUserDailys());
-  }, [dispatch]);
 
   const clickDaily = (daily) => {
     setCurrentUserDaily(daily);
@@ -91,9 +64,8 @@ const PersonalStats = ({ navigation }) => {
 };
 
 PersonalStats.propTypes = {
-  navigation: PropTypes.shape({
-    addListener: PropTypes.func,
-  }),
+  userDailys: PropTypes.any,
+  isLoadingUserDailys: PropTypes.bool,
 };
 
 export default PersonalStats;
