cd5edf77216fb8e71cf873bc7590acf43d8bd4ce
Jason Wang <jasonw80702@gmail.com>
2022-05-29 23:52:17 -0700

    feat(global_stats): setup basic global stats * NB: to test this, need to use android

--- components/stats/GlobalStats.js
+++ components/stats/GlobalStats.js
@@ -1,11 +1,36 @@
-import { Text } from '@rneui/base';
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Text } from '@rneui/base';
 import { View } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const GlobalStats = () => {
+  const [date, setDate] = useState(new Date());
+  const [showModal, setShowModal] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShowModal(false);
+
+    if (event?.type === 'dismissed') {
+      setDate(date);
+      return;
+    }
+
+    setDate(currentDate);
+  };
+
   return (
     <View>
-      <Text>Global</Text>
+      <Text>Global Stats</Text>
+      <Text>selected: {date?.toLocaleString()}</Text>
+      <Button title="Open" onPress={() => setShowModal(true)} />
+      {showModal && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date || new Date()}
+          onChange={onChange}
+        />
+      )}
     </View>
   );
 };
