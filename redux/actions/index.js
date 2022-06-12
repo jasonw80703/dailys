3a1386037107f73ba1c13227ec21ff9c9e971194
Jason Wang <jasonw80702@gmail.com>
2022-06-11 18:13:07 -0700

    refactor(redux_actions): refactor arrow functions

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -20,88 +20,72 @@ import {
   where,
 } from 'firebase/firestore';
 
-export const fetchUser = () => {
-  return (dispatch) => {
-    getDoc(doc(db, 'users', auth.currentUser.uid)).then((snapshot) => {
-      // console.log(auth.currentUser.uid);
-      if (snapshot.exists) {
-        dispatch({ type: USER_STATE_CHANGE, currentUser: snapshot.data() });
-      } else {
-        console.log('User does not exist.');
-      }
-    });
-  };
-};
-
-export const setUserLoading = () => {
-  return (dispatch) => {
-    dispatch({ type: USER_LOADING });
-  };
-};
-
-export const clearStore = () => {
-  return (dispatch) => {
-    dispatch({ type: CLEAR_DATA });
-  };
-};
-
-export const fetchUserDailys = () => {
-  return async (dispatch) => {
-    const userDailysRef = collection(db, 'userdailys');
-    const userDailysQuery = query(
-      userDailysRef,
-      where('userId', '==', auth.currentUser.uid),
-    );
-    const querySnapshot = await getDocs(userDailysQuery);
-    const userDailys = [];
-    if (!querySnapshot.empty) {
-      querySnapshot.forEach((doc) => {
-        const data = doc.data();
-        sort(userDailys, data, doc.id);
-      });
-    }
-    dispatch({ type: FETCH_USER_DAILYS, data: userDailys });
-  };
-};
-
-export const setUserDailysLoading = () => {
-  return (dispatch) => {
-    dispatch({ type: USER_DAILYS_LOADING });
-  };
-};
-
-export const fetchDailys = (date) => {
-  return async (dispatch) => {
-    const daily = await getDoc(doc(db, 'dailys', date));
-    if (daily.exists()) {
-      dispatch({ type: FETCH_DAILYS, data: daily.data(), date });
+export const fetchUser = () => dispatch => {
+  getDoc(doc(db, 'users', auth.currentUser.uid)).then((snapshot) => {
+    // console.log(auth.currentUser.uid);
+    if (snapshot.exists) {
+      dispatch({ type: USER_STATE_CHANGE, currentUser: snapshot.data() });
     } else {
-      dispatch({ type: FETCH_DAILYS_ERROR, error: 'No Daily found for this date.' });
+      console.log('User does not exist.');
     }
-  };
+  });
 };
 
-export const setDailysLoading = () => {
-  return (dispatch) => {
-    dispatch({ type: FETCH_DAILYS_LOADING });
-  };
+export const setUserLoading = () => dispatch => {
+  dispatch({ type: USER_LOADING });
 };
 
-export const fetchUserDailysByDate = (date) => {
-  return async (dispatch) => {
-    const userDailysRef = collection(db, 'userdailys');
-    const userDailysQuery = query(
-      userDailysRef,
-      where('date', '==', date),
-    );
-    const querySnapshot = await getDocs(userDailysQuery);
-    const userDailys = [];
-    if (!querySnapshot.empty) {
-      querySnapshot.forEach((doc) => {
-        const data = doc.data();
-        userDailys.push(data);
-      });
-    }
-    dispatch({ type: FETCH_USER_DAILYS_GLOBAL, data: userDailys, date });
-  };
+export const clearStore = () => dispatch => {
+  dispatch({ type: CLEAR_DATA });
+};
+
+export const fetchUserDailys = () => async dispatch => {
+  const userDailysRef = collection(db, 'userdailys');
+  const userDailysQuery = query(
+    userDailysRef,
+    where('userId', '==', auth.currentUser.uid),
+  );
+  const querySnapshot = await getDocs(userDailysQuery);
+  const userDailys = [];
+  if (!querySnapshot.empty) {
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      sort(userDailys, data, doc.id);
+    });
+  }
+  dispatch({ type: FETCH_USER_DAILYS, data: userDailys });
+};
+
+export const setUserDailysLoading = () => dispatch => {
+  dispatch({ type: USER_DAILYS_LOADING });
+};
+
+export const fetchDailys = (date) => async dispatch => {
+  const daily = await getDoc(doc(db, 'dailys', date));
+  if (daily.exists()) {
+    dispatch({ type: FETCH_DAILYS, data: daily.data(), date });
+  } else {
+    dispatch({ type: FETCH_DAILYS_ERROR, error: 'No Daily found for this date.' });
+  }
+};
+
+export const setDailysLoading = () => dispatch => {
+  dispatch({ type: FETCH_DAILYS_LOADING });
+};
+
+export const fetchUserDailysByDate = (date) => async dispatch => {
+  const userDailysRef = collection(db, 'userdailys');
+  const userDailysQuery = query(
+    userDailysRef,
+    where('date', '==', date),
+  );
+  const querySnapshot = await getDocs(userDailysQuery);
+  const userDailys = [];
+  if (!querySnapshot.empty) {
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      userDailys.push(data);
+    });
+  }
+  dispatch({ type: FETCH_USER_DAILYS_GLOBAL, data: userDailys, date });
 };
