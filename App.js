3dab8f8bfe5a07d08f43c211795d612e361a73f2
Jason Wang <jasonw80702@gmail.com>
2022-06-11 18:23:16 -0700

    refactor(redux): factor store out to separate file

--- App.js
+++ App.js
@@ -10,12 +10,8 @@ import Login from './components/auth/Login';
 import ForgotPassword from './components/auth/ForgotPassword';
 import { auth } from './firebase';
 
-// Redux
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './redux/reducers';
-import thunk from 'redux-thunk';
-const store = configureStore({ reducer: rootReducer, middleware: [thunk] });
+import { store } from './redux/store';
 
 const Stack = createNativeStackNavigator();
 
